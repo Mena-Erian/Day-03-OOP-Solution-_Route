b4b289ac9ef47984a52a4f4a0fcf5247be12e24c
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-20 17:43:15 +0300

    Q4 Employee Class

---
+++ Assignment/Part 1/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Part_1
+{
+    internal class Employee
+    {
+        public virtual void Work()
+        {
+            Console.WriteLine("Employee is Working");
+        }
+    }
+    internal class Manger : Employee
+    {
+        public new void Work()
+        {
+            Console.WriteLine("Manager is Managing");
+        }
+
+    }
+}
