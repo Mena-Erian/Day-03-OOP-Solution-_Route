11151c2b66f999d4ba72532931b38427ba6dcfb0
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-20 17:30:55 +0300

    Q2 Rectangle Class

---
+++ Assignment/Part 1/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Part_1
+{
+    internal class Rectangle
+    {
+        public Rectangle()
+        {
+            Width = 0; Height = 0;
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public Rectangle(int num) : this(num, num)
+        {
+
+        }
+
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
+
