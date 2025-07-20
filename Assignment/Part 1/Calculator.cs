289a35871d6dd56aa88bb6148a5e9f6e2868394d
Mena Erian <mena3koko2karas1@gmail.com>
2025-07-20 17:25:49 +0300

    Q1

---
+++ Assignment/Part 1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Part_1
+{
+    internal class Calculator
+    {
+        public static int Add(int n1, int n2)
+        { return n1 + n2; }
+        public static int Add(int n1, int n2, int n3)
+        { return n1 + n2 + n3; }
+        public static double Add(double n1, double n2)
+        { return n1 + n2; }
+    }
+}
